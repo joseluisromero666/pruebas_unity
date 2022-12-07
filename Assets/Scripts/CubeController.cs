2679bef92ba9f53a5e5ded609bdd11994eb1da2e
José Luis Romero Moreno <josel.romerom@konradlorenz.edu.co>
2022-12-07 17:38:10 -0500

    first commit

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class CubeController : MonoBehaviour
+{
+    Material material;
+
+    public Slider mainSliderX;
+
+    public Slider mainSliderY;
+
+    public Slider mainSliderZ;
+
+    public Slider mainSliderT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void RotarCubo()
+    {
+        transform.Rotate(new Vector3(45, 45, 45));
+    }
+
+    public void EscalarCubo(float value)
+    {
+        transform.localScale = new Vector3(value, value, value);
+    }
+
+    public void CambiarColor(int opcion)
+    {
+        Debug.Log("Paramentro " + opcion);
+        switch (opcion)
+        {
+            case 0:
+                Debug.Log("Opcion 1");
+                material.color = Color.green;
+                break;
+            case 1:
+                Debug.Log("Opcion 2");
+                material.color = Color.red;
+                break;
+            case 2:
+                Debug.Log("Opcion 3");
+                material.color = Color.yellow;
+                break;
+        }
+    }
+
+    public void CambiarX(float x)
+    {
+        transform.position =
+            new Vector3(x, transform.position.y, transform.position.z);
+    }
+
+    public void CambiarY(float y)
+    {
+        transform.position =
+            new Vector3(transform.position.x, y, transform.position.z);
+    }
+
+    public void CambiarZ(float z)
+    {
+        transform.position =
+            new Vector3(transform.position.x, transform.position.y, z);
+    }
+
+    public void Reset()
+    {
+      //  Debug.Log(transform.rotation.x);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.position = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(1, 1, 1);
+        mainSliderX.value = 0;
+        mainSliderY.value = 0;
+        mainSliderZ.value = 0;
+        mainSliderT.value = 1;
+    }
+    public void SalirAplicacion(){
+        Debug.Log("Saliendo de La app");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying=false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    // private Rigidbody rb;
+    //  public float speed;
+    public Transform particulas;
+
+    private ParticleSystem sistemaParticulas;
+
+    private Vector3 posicion;
+
+    public int recolector = 0;
+
+    private AudioSource audioRecoleccion;
+
+    public Text textoContador;
+
+    public Text textoGanador;
+
+    public GameObject CuboMovible;
+
+    public GameObject[] cuboDesaparecer;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+    }
+
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+        sistemaParticulas = particulas.GetComponent<ParticleSystem>();
+        sistemaParticulas.Stop();
+        audioRecoleccion = GetComponent<AudioSource>();
+
+        // textoContador.text = "Contador : " + recolector.ToString();
+        StartCoroutine("Movimiento");
+        cuboDesaparecer = GameObject.FindGameObjectsWithTag("Recolectable");
+        StartCoroutine("Desaparecer");
+        StartCoroutine("Nuevo");
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // rb.AddForce(movimiento * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
+            posicion = other.gameObject.transform.position;
+            particulas.position = posicion;
+            sistemaParticulas = particulas.GetComponent<ParticleSystem>();
+            sistemaParticulas.Play();
+
+            other.gameObject.SetActive(false);
+            StartCoroutine(DetenerParticulas(sistemaParticulas));
+
+            //  Destroy(other.gameObject);
+            recolector += 1;
+            if (recolector == 12)
+            {
+                textoGanador.text = "¡Nivel Completado!";
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                Debug.Log (recolector);
+                audioRecoleccion.Play();
+                textoContador.text = "Contador : " + recolector.ToString();
+            }
+        }
+    }
+
+    public IEnumerator DetenerParticulas(ParticleSystem part)
+    {
+        yield return new WaitForSecondsRealtime(5);
+        part.Stop();
+    }
+
+    public IEnumerator Movimiento()
+    {
+        for (; ; )
+        {
+            if (
+                Vector3
+                    .Distance(transform.position,
+                    CuboMovible.transform.position) <
+                6f
+            )
+            {
+                CuboMovible.transform.position =
+                    Vector3
+                        .Lerp(CuboMovible.transform.position,
+                        new Vector3(Random.Range(-10.0f, 10.0f),
+                            0.97f,
+                            Random.Range(-10.0f, 10.0f)),
+                        10f * Time.deltaTime);
+            }
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
+    }
+
+    public IEnumerator Desaparecer()
+    {
+        for (; ; )
+        {
+            yield return new WaitForSecondsRealtime(10);
+            cuboDesaparecer[12].gameObject.SetActive(false);
+        }
+    }
+
+    public IEnumerator Nuevo()
+    {
+        int contador = 0;
+        for (; ; )
+        {
+            yield return new WaitForSecondsRealtime(3);
+            cuboDesaparecer[13].gameObject.SetActive(false);
+            yield return new WaitForSecondsRealtime(5);
+            cuboDesaparecer[13].gameObject.SetActive(true);
+            if (contador == 0)
+            {
+                cuboDesaparecer[13].GetComponent<Renderer>().material.color =
+                    Color.green;
+            }
+            if (contador == 1)
+            {
+                cuboDesaparecer[13].GetComponent<Renderer>().material.color =
+                    Color.black;
+            }
+            if (contador == 2)
+            {
+                cuboDesaparecer[13].GetComponent<Renderer>().material.color =
+                    Color.red;
+            }
+            if (contador == 3)
+            {
+                cuboDesaparecer[13].GetComponent<Renderer>().material.color =
+                    Color.white;
+            }
+            if (contador == 4)
+            {
+                cuboDesaparecer[13].GetComponent<Renderer>().material.color =
+                    Color.blue;
+            }
+
+            contador += 1;
+            if (contador == 5)
+            {
+                contador = 0;
+            }
+        }
+    }
+}
